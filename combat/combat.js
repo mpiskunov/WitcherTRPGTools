54f9bb2cc6b7497609e06dc71b02081741cfd3c8
Matthew Piskunov <Mpiskunov@explorica.com>
2019-06-18 14:52:09 -0400

    Completed Workflow Visual

--- combat/combat.js
+++ combat/combat.js
@@ -1,51 +1,110 @@
 "use strict";
-$(document).ready(function() {
+$(document).ready(function () {
   // Initialize Common Elements
-  let divStart = $("#divStart");
-  let divAttackAim = $("#divAttackAim");
-  let divAttackRoll = $("#divAttackRoll");
-  let divDefenseRoll = $("#divDefenseRoll");
-  let divResult = $("#divResult");
-  let divBottomButtons = $("#divBottomButtons");
-  let btnStart = $("#btnStart");
-  let divBodyParts = $("#divBodyParts");
-  let btnPrev = $("#btnPrev");
-  let btnNext = $("#btnNext");
-  let rbgAiming = $("#rbgAiming");
-  let aiming = false;
+  let isAiming = false;
   let aimingMod = 0;
-  let optBodyParts = $("#optBodyParts");
+  let baseAttack = 0, modifierAttack = 0;
+  let baseDefense = 0, modifierDefence = 0;
   // Removes the hiding Bootstrap class to show element.
   const DisplayElement = el => el.removeClass("d-none");
   // Adds the hiding Bootstrap class to hide the element.
   const HideElement = el => el.addClass("d-none");
 
   // Start Logic
-  DisplayElement(divStart);
+  DisplayElement($("#divStart"));
 
-  btnStart.click(e => {
-    HideElement(divStart);
-    DisplayElement(divAttackAim);
-    DisplayElement(divBottomButtons);
-    DisplayElement(btnPrev);
-  });
+  $("#btnStart").click(e => {
+    HideElement($("#divStart"));
+    DisplayElement($("#divAttackAim"));
+    DisplayElement($("#divBottomButtons"));
+    DisplayElement($("#btnPrev"));
+    HideElement($("#btnNextBody"));
+    if (isAiming) {
 
-  rbgAiming.on("change", () => {
-    //Value if attacker is aiming or not
-    aiming = $("input[name=aiming]:checked").val();
-    if (aiming == true) {
-      console.log(aiming);
-      DisplayElement(divBodyParts);
-    } else {
-      console.log(aiming);
-      HideElement(divBodyParts);
+      DisplayElement($("#btnNextBody"));
+      (aimingMod == 0) ? HideElement($("#btnNextBody")) : DisplayElement($("#btnNextBody"));
     }
-    // Display Next Button
-    DisplayElement(btnNext);
+    else {
+
+    }
   });
 
-  btnNext.click(e => {
-    //console.log($(".jumbotron:not(.d-none)")[0].id);
-    console.log(optBodyParts.children("option:selected").val());
+  $("#rbgAiming").on("change", () => {
+    //Value if attacker is aiming or not
+    isAiming = $("input[name=aiming]:checked").val();
+    // Display dropdown for body list
+    if (isAiming == 1) {
+      DisplayElement($("#divBodyParts"));
+      (aimingMod == 0) ? HideElement($("#btnNextBody")) : DisplayElement($("#btnNextBody"));
+    } else {
+      HideElement($("#divBodyParts"));
+      DisplayElement($("#btnNextBody"));
+    }
+  });
+
+  $("#optBodyParts").on("change", () => {
+    aimingMod = $("#optBodyParts").children("option:selected").val();
+    // If it is not default value, show next button
+    (aimingMod != 0) ? DisplayElement($("#btnNextBody")) : HideElement($("#btnNextBody"));
+  });
+
+  // Start <- Aim
+  $("#btnPrevBody").click(e => {
+    HideElement($("#divAttackAim"));
+    DisplayElement($("#divStart"));
+  });
+
+  // Aim -> Attack Roll
+  $("#btnNextBody").click(e => {
+    HideElement($("#divAttackAim"));
+    DisplayElement($("#divAttackRoll"));
+  });
+
+  // Aim <- Attack Roll
+  $("#btnPrevAttRoll").click(e => {
+    HideElement($("#divAttackRoll"));
+    DisplayElement($("#divAttackAim"));
+  });
+
+  // Attack Roll -> Defense Roll
+  $("#btnNextAttRoll").click(e => {
+
+    if (isNaN($("#inputAttBase").val()) || $("#inputAttBase").val() == "")
+      return;
+    if (isNaN($("#inputAttMod").val()) || $("#inputAttMod").val() == "")
+      return;
+
+    let baseVal = $("#inputAttBase").val();
+    let modVal = $("#inputAttMod").val();
+
+    HideElement($("#divAttackRoll"));
+    DisplayElement($("#divDefenseRoll"));
+  });
+
+  // Attack Roll <- Defense Roll
+  $("#btnPrevDefRoll").click(e => {
+    HideElement($("#divDefenseRoll"));
+    DisplayElement($("#divAttackRoll"));
+  });
+
+  // Defense Roll -> Result
+  $("#btnNextDefRoll").click(e => {
+
+    if (isNaN($("#inputDefBase").val()) || $("#inputDefBase").val() == "")
+      return;
+    if (isNaN($("#inputDefMod").val()) || $("#inputDefMod").val() == "")
+      return;
+
+    let baseVal = $("#inputDefBase").val();
+    let modVal = $("#inputDefMod").val();
+
+    HideElement($("#divDefenseRoll"));
+    DisplayElement($("#divResult"));
+  });
+
+  // Defense Roll <- Result
+  $("#btnPrevResult").click(e => {
+    HideElement($("#divResult"));
+    DisplayElement($("#divDefenseRoll"));
   });
 });
